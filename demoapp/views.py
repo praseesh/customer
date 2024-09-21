aa0c33233b71f06464a9e5395224c931abfaa2be
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-09-21 22:16:11 +0530

    Update views.py

--- demoapp/views.py
+++ demoapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import CustomerForm
-# # Create your views here.
 
 def index(request):
     custform=CustomerForm()
